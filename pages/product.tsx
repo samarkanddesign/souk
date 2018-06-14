ca3969f85a13974489ab57b7e8d048c0494ba589
harrygr <harry@grumbar.me>
2018-06-14 23:05:45 +0100

    Add product query

--- pages/product.tsx
+++ pages/product.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import Layout from 'components/layout';
 import { withRouter, SingletonRouter } from 'next/router';
+import gql from 'graphql-tag';
 
 interface Props {
   router: SingletonRouter;
 }
+
+const productBySlug = gql`
+  query SingleProduct($slug: String) {
+    product(slug: $slug) {
+      name
+      slug
+      description
+      price
+      salePrice
+    }
+  }
+`;
 
 function Product(props: Props) {
   return (
